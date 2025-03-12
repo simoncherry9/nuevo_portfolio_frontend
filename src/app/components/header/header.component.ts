3403628c5a905d9d3bc45b52cc3ea02afe716dd4
Simón Cherry <hacker22.pentesting@proton.me>
2025-03-12 00:26:52 -0300

    Implementacion de admin
    Implementacion de admin panel y aplicacion de personalizacion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service'; // Asegúrate de importar el servicio correctamente
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  logout() {
+    // Llamada al método logout del AuthService
+    this.authService.logout();
+
+    // Redirigir a la página de login después de cerrar sesión
+    this.router.navigate(['/login']);
+  }
+
+  goToMessages() {
+    // Lógica para ir a la página de mensajes
+    console.log('Go to messages clicked');
+    this.router.navigate(['/mensajes']);
+  }
 }
